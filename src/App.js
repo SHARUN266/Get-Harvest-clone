63c1d56c06a3d16db0351ab4a05ac00f19848003
SHARUN266 <sharunattari266@gmail.com>
2022-09-30 11:40:13 +0530

    Day-4

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import Dashboard from "./Components/Arsalan/Dashboard/Dashboard";
 import Footer from "./Sharun/Footer/Footer";
 import Navbar from "./Sharun/Navbar/Navbar";
 import Arsalan from "./Components/Arsalan/Arsalan";
+import SignUp from "./Sharun/SignUp/SignUp";
+import SimpleCard from "./Sharun/SignIn/SignIn";
 
 function App() {
   return (
     <>
       <Navbar />
       {/* <Dashboard /> */}
-      <Arsalan />
+    
+      <SignUp/>
+      <SimpleCard/>
       <Footer />
     </>
   );
