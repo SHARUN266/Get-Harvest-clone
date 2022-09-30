6cc9ef6542a787989e35f668d354d0f3d52101bf
arsalanshaikhh <arsalan4coder@gmail.com>
2022-10-01 03:51:18 +0530

    Integrations

--- src/Components/Arsalan/Arsalan.jsx
+++ src/Components/Arsalan/Arsalan.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Dashboard from "./Dashboard/Dashboard";
 import Featurs from "./Features/Featurs";
+import Integrations from "./Integrations/Integrations";
 
 const Arsalan = () => {
   return (
     <>
       <Dashboard />
       <Featurs />
+      <Integrations />
     </>
   );
 };
