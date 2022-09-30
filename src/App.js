052b490939222f94508350ee758c63f830770a0f
arsalanshaikhh <arsalan4coder@gmail.com>
2022-09-30 07:15:59 +0530

    day 3

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
-
-
-
-
-
-
 import { Box, Heading } from "@chakra-ui/react";
+import Dashboard from "./Components/Dashboard/Dashboard";
 import Footer from "./Sharun/Footer/Footer";
 import Navbar from "./Sharun/Navbar/Navbar";
 
 function App() {
-  return <>
-  <Navbar/>
- 
-  <Footer/>
-  </>;
-
+  return (
+    <>
+      <Navbar />
+      <Dashboard />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
