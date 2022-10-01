6bc70d9ec2586aefaaedf9e7817a1a34ea16e57c
arsalanshaikhh <arsalan4coder@gmail.com>
2022-10-01 11:31:37 +0530

    Tracking

--- src/Components/Arsalan/Arsalan.jsx
+++ src/Components/Arsalan/Arsalan.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Dashboard from "./Dashboard/Dashboard";
 import Featurs from "./Features/Featurs";
 import Integrations from "./Integrations/Integrations";
+import Learning from "./Learning/Learning";
+import Tracking from "./Tracking/Tracking";
 
 const Arsalan = () => {
   return (
@@ -9,6 +11,8 @@ const Arsalan = () => {
       <Dashboard />
       <Featurs />
       <Integrations />
+      <Learning />
+      <Tracking />
     </>
   );
 };
